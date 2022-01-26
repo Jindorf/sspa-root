178087f2d221331eef8d3cfa523904bbef15cc5e
Jannik Indorf <jannik.indorf@allgeier-it.de>
2022-01-26 11:22:02 +0100

    update

---
+++ root-application.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+import * as singleSpa from 'single-spa';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+function showWhenAnyOf(routes) {
+  return function (location) {
+    return routes.some((route) => location.pathname === route);
+  };
+}
+
+function showWhenPrefix(routes) {
+  return function (location) {
+    return routes.some((route) => location.pathname.startsWith(route));
+  };
+}
+
+function showExcept(routes) {
+  return function (location) {
+    return routes.every((route) => location.pathname !== route);
+  };
+}
+singleSpa.registerApplication(
+  'login',
+  () => import('sspa-auth'),
+  showWhenAnyOf(['/']),
+);
+
+singleSpa.registerApplication(
+  'vue-app',
+  () => import('single-spa-vue-app'),
+  showWhenAnyOf(['/vue'])
+)
+
+singleSpa.start();
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = () => ({
+    mode: "production",
+    entry: {
+        "root-application": "root-application.js",
+    },
+    output: {
+        publicPath: "/",
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['css-loader'],
+              },
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+        ],
+    },
+    resolve: {
+        modules: [__dirname, "node_modules"],
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ["dist"],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+            inject: false,
+        }),
+    ],
+    devtool: "source-map",
+    externals: [],
+    devServer: {
+        historyApiFallback: true,
+    },
+})
